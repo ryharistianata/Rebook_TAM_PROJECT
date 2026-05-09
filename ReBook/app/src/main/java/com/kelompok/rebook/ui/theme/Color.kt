e3a0351909c859c8507d6dfdf5a4cd5c1d8b8c3b
Elsy Aliffia Sirony Putri <elsyyaliffiaaa@gmail.com>
2026-05-09 16:02:50 +0700

    mengatur palet warna aplikasi

--- ReBook/app/src/main/java/com/kelompok/rebook/ui/theme/Color.kt
+++ ReBook/app/src/main/java/com/kelompok/rebook/ui/theme/Color.kt
@@ -1,11 +1,42 @@
-package com.example.myapplication.ui.theme
+package com.kelompok.rebook.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Primary40      = Color(0xFF2D7D6F)
+val Primary20      = Color(0xFF004D40)
+val Primary80      = Color(0xFF7DCFBF)
+val Primary90      = Color(0xFFB2EBE0)
+val PrimaryContainer40 = Color(0xFFB2DFDB)
+val PrimaryContainer20 = Color(0xFF003731)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Secondary40      = Color(0xFFA0522D)
+val Secondary80      = Color(0xFFFFB68F)
+val SecondaryContainer = Color(0xFFFFDCC9)
+
+val Tertiary40       = Color(0xFF4051B5)
+val Tertiary80       = Color(0xFFBEC6FF)
+val TertiaryContainer = Color(0xFFDEE0FF)
+
+val PaperLight       = Color(0xFFFDFBF7)
+val InkDark          = Color(0xFF1A1C1B)
+val PaperDark        = Color(0xFF121414)
+
+val Neutral10        = Color(0xFF191C1B)
+val Neutral20        = Color(0xFF2D3130)
+val Neutral90        = Color(0xFFE0E3E1)
+val Neutral95        = Color(0xFFEFF1EF)
+val Neutral99        = Color(0xFFF5FBF8)
+val NeutralVariant30 = Color(0xFF3F4945)
+val NeutralVariant50 = Color(0xFF6F7975)
+val NeutralVariant80 = Color(0xFFBEC8C3)
+val NeutralVariant90 = Color(0xFFDAE5DF)
+
+val StatusAvailable  = Color(0xFF2E7D32)
+val StatusBorrowed   = Color(0xFFD84315)
+val StatusDonated    = Color(0xFF5E35B1)
+
+val ErrorColor       = Color(0xFFBA1A1A)
+val ErrorContainer   = Color(0xFFFFDAD6)
+
+val White            = Color(0xFFFFFFFF)
+val Black            = Color(0xFF000000)

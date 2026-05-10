851c6c58a43a284637d335d53f7bec864217855a
KharismaJaka <kjakaharum@gmail.com>
2026-05-10 12:31:34 +0700

    Memperbarui MainActivity

--- ReBook/app/src/main/java/com/kelompok/rebook/MainActivity.kt
+++ ReBook/app/src/main/java/com/kelompok/rebook/MainActivity.kt
@@ -1,47 +1,29 @@
-package com.example.myapplication
+package com.kelompok.rebook
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.myapplication.ui.theme.MyApplicationTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.kelompok.rebook.ui.navigation.ReBookNavGraph
+import com.kelompok.rebook.ui.theme.ReBookTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            var isDarkTheme by remember { mutableStateOf(false) }
+
+            ReBookTheme(darkTheme = isDarkTheme, dynamicColor = false) {
+                ReBookNavGraph(
+                    isDarkTheme = isDarkTheme,
+                    onThemeToggle = { isDarkTheme = !isDarkTheme }
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
     }
 }
